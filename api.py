821d67f2f37827ee099dab7d312f6b041c061b0d
Erton <ericsanchezjimenez35@gmail.com>
2022-04-10 23:54:42 +0200

    Added placeholder API functions
    Added placeholders for GET, PUSH and PUT methods

--- api.py
+++ api.py
@@ -28,3 +28,44 @@ def connect():
     except Exception as error:
         # TODO(Erton): Instead of print should become a notification in our GUI
         print(f"ERROR: Failed to connect to database.\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n")
+
+
+
+# GET
+def get_user(conn, key, param):
+    pass
+
+def get_requests(conn, key, param):
+    pass
+
+def get_chat(conn, key, param):
+    pass
+
+
+
+# POST
+def post_user(conn, user_id, user_name, user_pass):
+    # Friends, days and valoration are unnecessary
+    pass
+
+def post_requests(conn, from_who, type, desc):
+    pass
+
+def post_chat(conn, ids):
+    # Last is unnecessary
+    pass
+
+
+
+# PUT
+def put_user(conn, key, param, value):
+    pass
+
+def put_global(conn, key, param, value):
+    pass
+
+def put_requests(conn, key, param, value):
+    pass
+
+def put_chat(conn, key, param, value):
+    pass
